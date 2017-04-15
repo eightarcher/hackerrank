3d495f4ecbc8e5de093661d2fee185e1e1b3e517
eightarcher <mikeschaff@gmail.com>
2017-04-14 23:54:39 -0400

    Commit Simple Array Sum

---
+++ SimpleArraySum.py
@@ -0,0 +1,48 @@
+"""
+Given an array of  integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. 
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+Explanation
+
+We print the sum of the array's elements, which is: .1+2+3+4+10+11 = 31
+
+"""
+
+
+#sample code
+
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+#a = str("2 3 4 5 6 98")
+
+def arrsum(ray):
+
+    toats = 0
+    for num in ray:
+        toats = toats + num
+        #print(toats)
+    #print(toats)
+    print(toats)
+
+arrsum(arr)
